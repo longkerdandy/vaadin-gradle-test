91184bbfcb6f3dd45186e0b3eb362bf093607a41
LongkerDandy <longkerdandy@gmail.com>
2020-07-12 14:38:54 +0800

    Demo project

---
+++ vaadin-module/src/main/java/cn/royalnanny/backend/vaadin/GreetService.java
@@ -0,0 +1,12 @@
+package cn.royalnanny.backend.vaadin;
+
+public class GreetService {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+}
